2de533d2f163bc10bca6814deb3d37bdf0f6b3d1
visitas-cloud <visitas.custodio@gmail.com>
2026-08-08 22:16:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuração do Supabase — projeto "Lojas GCM"
 const SUPABASE_CONFIG = {
-  url: "https://tecpyernkybpjzavossp.supabase.co/rest/v1/",
+  url: "https://tecpyernkybpjzavossp.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlY3B5ZXJua3licGp6YXZvc3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMjcwNzcsImV4cCI6MjEwMTgwMzA3N30.H5Jp7LF0-H8WWg7G-7J9bGrU0X_7ThBpMo-aLN3NkVw"
 };
